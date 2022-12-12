d4ec6ed5ec05322013a333cb675754f7021860e2
Mahmoud Qasem <mqasim@kn-it.com>
2022-12-13 00:50:51 +0200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MinapharmTask.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
